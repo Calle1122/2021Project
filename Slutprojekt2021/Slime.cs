e2b18150a76b1ce3abda1b7ca25d867a990306d2
Calle1122 <60339644+Calle1122@users.noreply.github.com>
2021-03-23 14:46:43 +0100

    jobbar på basklassen

--- Slutprojekt2021/Slime.cs
+++ Slutprojekt2021/Slime.cs
@@ -4,5 +4,41 @@ namespace Slutprojekt2021
 {
     public class Slime
     {
+        public int hp;
+        public int exp;
+        public int level;
+        public int damage;
+        public int defense;
+
+        protected int expRequired;
+        protected bool isAlive;
+
+        public void addExp(int expAmount){
+            exp = exp + expAmount;
+
+            if(exp > expRequired){
+                exp = exp - expRequired;
+                expRequired = expRequired * 2;
+                level++;
+            }
+        }
+
+        public void takeDamage(int damageAmount){
+            damageAmount = damageAmount - defense;
+
+            if(damageAmount < 0){
+                damageAmount = 0;
+            }
+
+            hp = hp - damageAmount;
+            if(hp < 0){
+                hp = 0;
+            }
+
+            if(hp == 0){
+                isAlive = false;
+            }
+        }
+
     }
 }
