fe8c8c7a144ba29d64dbacb1e72545bd8d8bf030
Calle1122 <60339644+Calle1122@users.noreply.github.com>
2021-03-23 14:29:27 +0100

    fixat external terminal och skapat en basklass

--- Slutprojekt2021/Program.cs
+++ Slutprojekt2021/Program.cs
@@ -7,6 +7,7 @@ namespace Slutprojekt2021
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }
---
+++ Slutprojekt2021/Slime.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Slutprojekt2021
+{
+    public class Slime
+    {
+    }
+}
